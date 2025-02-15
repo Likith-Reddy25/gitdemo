6b4fe03f64fb4e586a31ce63f73285230685cc93
Likith-Reddy25 <likithreddy2508@gmail.com>
2025-02-15 22:52:00 +0530

    Updated

--- demo.java
+++ demo.java
@@ -1,7 +1,11 @@
-package gitdemo;
+import java.util.Scanner;
+
 public class demo{
     public static void main(String[] args)
     {
         System.out.println("Hello world!!!");
+        System.out.println("Enter a number");
+        Scanner input = new Scanner(System.in);
+        System.out.println(input.next());
     }
 }
